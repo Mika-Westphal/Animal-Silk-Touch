b8e4614f9cc6bbcd07b982be1694ccae39a8a60b
Mika Westphal <westphal.mika@freenet.de>
2019-02-20 20:16:39 +0100

    Changed normal color codes to ColorCode from bukkit/spigot

--- src/me/igamblenull/animalsilktouch/command/commandReloadConfig.java
+++ src/me/igamblenull/animalsilktouch/command/commandReloadConfig.java
@@ -1,5 +1,6 @@
 package me.igamblenull.animalsilktouch.command;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,20 +18,18 @@ public class commandReloadConfig implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(args.length != -1) {
-			if(args[0].equalsIgnoreCase("reload")) {
-				if(sender instanceof Player) {
-					Player p = (Player) sender;	
-					if(p.hasPermission("AnimalSilkTouch.reload")) {
-						plugin.reloadConfig();
-						p.sendMessage("§aAnimal Silk Touch reloaded!");
-					}else {
-						p.sendMessage("§4You don't have permission to use this command!");
-					}
-				}else {
+		if(args[0].equalsIgnoreCase("reload")) {
+			if(sender instanceof Player) {
+				Player p = (Player) sender;	
+				if(p.hasPermission("AnimalSilkTouch.reload")) {
 					plugin.reloadConfig();
-					sender.sendMessage("§aAnimal Silk Touch reloaded!");
+					p.sendMessage(ChatColor.RED + "Animal Silk Touch reloaded!");
+				}else {
+					p.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
 				}
+			}else {
+				plugin.reloadConfig();
+				sender.sendMessage(ChatColor.GREEN + "Animal Silk Touch reloaded!");
 			}
 		}
 		return true;
